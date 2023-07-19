a8e5c74985f3236d9d7c799882d4b65e678f7a9f
Nastya Sekerich <nastyasekerich@coherentsolutions.com>
2023-07-19 17:55:39 +0200

    add getStatic functions

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,50 @@
 import path from 'path';
 import fs from 'fs/promises';
+import Link from 'next/link';
 
 function HomePage(props) {
     const { products } = props;
-
-
 
   return (
     <ul>
         {products.map((product) => (
             <li key={product.id}>
-                {product.title}
+                <Link href={`/${product.id}`}>
+                    {product.title}
+                </Link>
             </li>
         ))}
     </ul>
   );
 }
 
-
-export async function getStaticProps() {
+export async function getStaticProps(context) {
+    console.log('Re-generated');
     const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
     const jsonData = await fs.readFile(filePath);
     const data = JSON.parse(jsonData);
 
-    return { props: {
-        products: data.products,
-    }};
+    if(!data) {
+        return {
+            redirect: {
+                destination: '/no-data',
+            },
+        }
+    }
+
+    if(data.products.length === 0) {
+        return {
+            notFound: true,
+        }
+    }
+
+    return {
+        props: {
+            products: data.products,
+        },
+        //will matter on production
+        revalidate: 120,
+    };
 }
 
 export default HomePage;
