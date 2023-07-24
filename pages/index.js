62aaf6a58684193197f0b6d1c781f4b5c33be244
Nastya Sekerich <nastyasekerich@coherentsolutions.com>
2023-07-24 12:45:10 +0200

    init: add basic project

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-function HomePage(props) {
+import { getFeaturedEvents } from '../dummy-data';
+import EventList from '../components/events/event-list';
+
+function HomePage() {
+  const featuredEvents = getFeaturedEvents();
+
   return (
-    <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
-    </ul>
+    <div>
+      <EventList items={featuredEvents} />
+    </div>
   );
 }
 
