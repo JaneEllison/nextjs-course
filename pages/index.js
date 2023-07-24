a4b54dbd770e527f0a5f2321aea4391dbd55c510
Nastya Sekerich <nastyasekerich@coherentsolutions.com>
2023-07-24 18:38:31 +0200

    init: init the project

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-function HomePage(props) {
+function HomePage() {
   return (
-    <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
-    </ul>
+    <div>
+      <h1>The Home Page</h1>
+    </div>
   );
 }
 
