b5676bed98bb0e344878a69f021a808c0f9c594d
Nastya Sekerich <nastyasekerich@coherentsolutions.com>
2023-07-25 12:07:17 +0200

    feat: add form to the page

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,8 +1,36 @@
+import fs from 'fs';
+import path from 'path';
+
 function handler(req, res) {
     //execute any server side code, will never be in frontend part
-    res.status(200).json({
-        message: 'This works',
-    });
+
+    if(req.method === 'POST') {
+        //extract data and stored to database
+        const email = req.body.email;
+        const feedbackText = req.body.text;
+
+        const newFeedback = {
+            id: new Date().toISOString(),
+            email: email,
+            text: feedbackText,
+        };
+
+        const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+        const fileData = fs.readFileSync(filePath);
+        const data = JSON.parse(fileData);
+
+        data.push(newFeedback);
+        fs.writeFileSync(filePath, JSON.stringify(data));
+
+        res.status(201).json({
+            message: 'Success!',
+            feedback: newFeedback
+        });
+    } else {
+        res.status(200).json({
+            message: 'This works',
+        });
+    }
 }
 
 export default handler;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,45 @@
+import { useRef } from 'react';
+
 function HomePage() {
+    const emailInput = useRef();
+    const feedbackInput = useRef();
+
+    const submitFormHandler = async (event) => {
+        event.preventDefault();
+
+        const email = emailInput.current.value;
+        const feedback = feedbackInput.current.value;
+        const reqBody = {
+            email,
+            text: feedback,
+        };
+
+        const response = await fetch('/api/feedback', {
+            method: 'POST',
+            body: JSON.stringify(reqBody),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = response.json();
+        console.log(data);
+    }
+
   return (
     <div>
       <h1>The Home Page</h1>
+        <form onSubmit={submitFormHandler}>
+            <div>
+                <label htmlFor='email'>Your email address</label>
+                <input type='email' id='email' ref={emailInput} />
+            </div>
+            <div>
+                <label htmlFor='feedback'>Your feedback</label>
+                <textarea rows='5' id='feedback' ref={feedbackInput} />
+            </div>
+            <button>Send feedback</button>
+        </form>
     </div>
   );
 }
