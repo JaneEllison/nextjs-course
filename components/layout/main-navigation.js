e7f136bed7c33a445c125fc0a37f892ebaaec2c1
Nastya Sekerich <nastyasekerich@coherentsolutions.com>
2023-08-02 10:08:45 +0200

    refactoring: fix project version && paths

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -5,7 +5,7 @@ import classes from './main-navigation.module.css';
 function MainNavigation() {
   return (
     <header className={classes.header}>
-      <Link href='/pages'>
+      <Link href='/'>
         <a>
           <div className={classes.logo}>Next Auth</div>
         </a>
@@ -13,10 +13,10 @@ function MainNavigation() {
       <nav>
         <ul>
           <li>
-            <Link href='/pages/auth'>Login</Link>
+            <Link href='/auth'>Login</Link>
           </li>
           <li>
-            <Link href='/pages/profile'>Profile</Link>
+            <Link href='/profile'>Profile</Link>
           </li>
           <li>
             <button>Logout</button>
